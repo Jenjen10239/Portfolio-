587816e41e46b7c1fd9a4f8dd28f23b77bf3f66c
Jenjen10239 <rockyguarin53@gmail.com>
2025-12-03 22:54:39 +0800

    Add files via upload

---
+++ script-1.js
@@ -0,0 +1,9 @@
+function showPage(pageId) {
+    let pages = document.querySelectorAll('.page');
+
+    pages.forEach(p => {
+        p.classList.remove('active');
+    });
+
+    document.getElementById(pageId).classList.add('active');
+}
